9a5f21c90159e6045582b909bd3edb39a0ff0015
malpp <malpp@users.noreply.github.com>
2017-07-20 13:43:15 -0400

    Updated default value of api and styled python code a bit

--- misc/python_script/main.py
+++ misc/python_script/main.py
@@ -4,12 +4,20 @@ import json
 import base64
 import yaml
 import zlib
+import os
+
 
 # Load config
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 config = yaml.safe_load(open("config.yml"))
 
 # Check for missing config keys
-config_vars = ['coveo_org_id', 'coveo_source_id', 'coveo_api_key',  'coveo_push_url', 'git_user', 'git_api_key']
+config_vars = [	'coveo_org_id',
+				'coveo_source_id',
+				'coveo_api_key',
+				'coveo_push_url',
+				'git_user',
+				'git_api_key']
 is_config_missing = False
 for config_var in config_vars:
 	if config_var not in config:
@@ -19,23 +27,27 @@ for config_var in config_vars:
 if is_config_missing:
 	exit('Some required keys are missing from config.yml.')
 
-### Github settings
+# Github settings
 user = config['git_user']
 git_url = "https://api.github.com/users/{0}/repos".format(user)
 git_auth = config['git_api_key']
 git_headers = {"Authorization": git_auth}
 
-### Push API settings
+# Push API settings
 org_id = config['coveo_org_id']
 source_id = config['coveo_source_id']
 coveo_auth = config['coveo_api_key']
-coveo_headers = {"Accept": "application/json", "Authorization" : coveo_auth, "Content-Type": "application/json"}
+coveo_headers = {
+	"Accept": "application/json", 
+	"Authorization": coveo_auth, 
+	"Content-Type": "application/json"
+}
 
-#Final data variable
+# Final data variable
 data = []
 
 # Get all repos for the user
-git_repo_request = requests.get(git_url, headers = git_headers)
+git_repo_request = requests.get(git_url, headers=git_headers)
 git_repo_data = json.loads(git_repo_request.text)
 
 print "Found {0} repo(s) for {1}".format(len(git_repo_data), user)
@@ -47,14 +59,14 @@ for repos in git_repo_data:
 
 	# Get all files in the repo
 	repo_contents_url = '/'.join(repos['contents_url'].split('/')[:-1])
-	repo_contents_request = requests.get(repo_contents_url, headers = git_headers)
+	repo_contents_request = requests.get(repo_contents_url, headers=git_headers)
 	repo_contents_data = json.loads(repo_contents_request.text)
 
 	# Loop through all the files in the repo
 	for repo_content in repo_contents_data:
 
 		# Find the 'extensions' folder
-		if('name' in  repo_content and 'type' in repo_content and repo_content['name'] == 'extensions' and repo_content['type'] == 'dir'):
+		if('name' in repo_content and 'type' in repo_content and repo_content['name'] == 'extensions' and repo_content['type'] == 'dir'):
 			
 			print "Found 'extensions' folder in {0}".format(repos['name'])
 
@@ -100,7 +112,7 @@ for repos in git_repo_data:
 						"required": reqData, 
 						"CompressedBinaryData": base64.b64encode(zlib.compress(extension_file_content)),
 						"url": extension_file['html_url'],
-						"FileExtension" : '.py'
+						"FileExtension": '.py'
 					})
 
 print "Sending {0} extension(s) to Coveo Index".format(len(data))
@@ -108,6 +120,6 @@ print "Sending {0} extension(s) to Coveo Index".format(len(data))
 # Send all results to the PUSH api
 for result in data:
 	r = requests.put("{0}/organizations/{1}/sources/{2}/documents?documentId={3}".format(config['coveo_push_url'], org_id, source_id, result['url']),
-					data=json.dumps(result), headers = coveo_headers)
+					data=json.dumps(result), headers=coveo_headers)
 
-	print "{0}: {1}".format(result['rawfilename'], r.status_code)
+	print "{0}: {1} - {2}".format(result['rawfilename'], r.status_code, r.text)
